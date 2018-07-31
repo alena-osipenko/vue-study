445172e3e96ae30f6d675fe5ca5afe3688aa083e
Alena Osipenko <alena.osipenko8@gmail.com>
2018-07-31 11:51:31 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Homework/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Homework from './components/Homework.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Homework,
   },
 };
 </script>
