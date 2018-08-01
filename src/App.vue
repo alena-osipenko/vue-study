ca379eea24dfeed5e448e6c4b636357d6bc60048
Alena Osipenko <alena.osipenko8@gmail.com>
2018-08-01 11:50:43 +0300

    Make second homework

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <Homework/>
+    <TableData/>
+    <Inputs/>
   </div>
 </template>
 
 <script>
 import Homework from './components/Homework.vue';
+import TableData from './components/TableData.vue';
+import Inputs from './components/Inputs.vue';
 
 export default {
   name: 'app',
   components: {
     Homework,
+    TableData,
+    Inputs,
   },
 };
 </script>
@@ -21,7 +26,7 @@ export default {
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
+  text-align left
   color #2c3e50
   margin-top 60px
 </style>
